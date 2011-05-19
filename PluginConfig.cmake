e2db13c76eba72987daffee70955826143f4d7dd
Jan Dolecek <juzna.cz@gmail.com>
2011-05-19 18:14:35 +0100

    autocreated firebreath project

---
+++ PluginConfig.cmake
@@ -0,0 +1,59 @@
+#/**********************************************************\ 
+#
+# Auto-Generated Plugin Configuration file
+# for linphone-js
+#
+#\**********************************************************/
+
+set(PLUGIN_NAME "linphone")
+set(PLUGIN_PREFIX "LIN")
+set(COMPANY_NAME "juznacz")
+
+# ActiveX constants:
+set(FBTYPELIB_NAME linphoneLib)
+set(FBTYPELIB_DESC "linphone 1.0 Type Library")
+set(IFBControl_DESC "linphone Control Interface")
+set(FBControl_DESC "linphone Control Class")
+set(IFBComJavascriptObject_DESC "linphone IComJavascriptObject Interface")
+set(FBComJavascriptObject_DESC "linphone ComJavascriptObject Class")
+set(IFBComEventSource_DESC "linphone IFBComEventSource Interface")
+set(AXVERSION_NUM "1")
+
+# NOTE: THESE GUIDS *MUST* BE UNIQUE TO YOUR PLUGIN/ACTIVEX CONTROL!  YES, ALL OF THEM!
+set(FBTYPELIB_GUID b41b1b86-3335-5941-9a2d-2daec3c12907)
+set(IFBControl_GUID 9583c828-dabf-5c76-9433-f5d6dbc71cf9)
+set(FBControl_GUID 398f576f-7f71-5a87-86bd-2f6938fb9311)
+set(IFBComJavascriptObject_GUID 06ffcfcc-95be-54f9-b36b-f4589d3cb45a)
+set(FBComJavascriptObject_GUID 2fd550d4-b43d-50ac-9b18-2d167bdeb01b)
+set(IFBComEventSource_GUID 46419614-0203-5151-811e-de12a6b65bbf)
+
+# these are the pieces that are relevant to using it from Javascript
+set(ACTIVEX_PROGID "juznacz.linphone")
+set(MOZILLA_PLUGINID "juzna.cz/linphone")
+
+# strings
+set(FBSTRING_CompanyName "juzna.cz")
+set(FBSTRING_FileDescription "Linphone (VoIP client) binding for JavaScript")
+set(FBSTRING_PLUGIN_VERSION "1.0.0.0")
+set(FBSTRING_LegalCopyright "Copyright 2011 juzna.cz")
+set(FBSTRING_PluginFileName "np${PLUGIN_NAME}.dll")
+set(FBSTRING_ProductName "linphone-js")
+set(FBSTRING_FileExtents "")
+set(FBSTRING_PluginName "linphone-js")
+set(FBSTRING_MIMEType "application/x-linphone")
+
+# Uncomment this next line if you're not planning on your plugin doing
+# any drawing:
+
+set (FB_GUI_DISABLED 1)
+
+# Mac plugin settings. If your plugin does not draw, set these all to 0
+set(FBMAC_USE_QUICKDRAW 0)
+set(FBMAC_USE_CARBON 0)
+set(FBMAC_USE_COCOA 0)
+set(FBMAC_USE_COREGRAPHICS 0)
+set(FBMAC_USE_COREANIMATION 0)
+set(FBMAC_USE_INVALIDATINGCOREANIMATION 0)
+
+# If you want to register per-machine on Windows, uncomment this line
+#set (FB_ATLREG_MACHINEWIDE 1)
