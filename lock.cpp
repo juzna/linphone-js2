7db28c67dc477ef5fb026378afbbfea7da1f1547
Jan Dolecek <juzna.cz@gmail.com>
2011-05-22 00:13:58 +0100

    refactored lock class

---
+++ lock.cpp
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include "lock.h"
+
+// Initialize a lock
+Lock::Lock(pthread_mutex_t *plock, char *from) {
+	if(from) {
+	  printf("locking mutex: ");
+	}
+	
+	pthread_mutex_lock(_lock = plock);
+	
+	if(_from = from) {
+	  printf("locked from %s\n", from); 
+	}
+}
+ 
+  // Destroy lock (unlock it)
+Lock::~Lock() {
+	if(_from) {
+	  printf("unlocking mutex: %s\n", _from);
+	}
+
+	pthread_mutex_unlock(_lock); 
+}
