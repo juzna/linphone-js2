e9d5286c0b0b83663566d48a1987708d1b4edeb1
Jan Dolecek <juzna.cz@gmail.com>
2011-05-23 00:51:35 +0100

    added submodules and build script

---
+++ doc/build.sh
@@ -0,0 +1,38 @@
+# remove old and buggy version of linphone from your system (version 3 from Ubuntu 9.10)
+sudo apt-get remove liblinphone3 || sudo apt-get remove liblinphone2
+
+# install basic tools
+sudo apt-get install g++ cmake make autoconf gcc git-core wget autoconf libtool intltool
+
+# install dependencies for linphone core
+sudo apt-get install libspeex-dev libspeexdsp-dev libxml2-dev libosip2-dev libexosip2-dev \
+	libavcodec-dev libswscale-dev libsdl-dev libgtk2-dev libx11-dev libxv-dev libx4l-dev
+
+# prepare dependencies
+#sudo apt-get build-dep liblinphone2 || sudo apt-get build-dep liblinphone3
+
+# download submodules
+git submodule update --init --recursive
+
+# we want debug version
+export CFLAGS='-ggdb3 -fPIC'
+
+
+# build linphone
+cd 3rdParty/linphone
+./autogen.sh
+./configure --enable-gtk_ui=no && make  || exit 1
+#sudo make install || exit 1  #optional
+cd ../..
+
+# prepare firebreath build
+3rdParty/firebreath/prepmake.sh ./ build/
+
+
+# build plugin
+cd build/
+make linphone
+
+# copy generated plugin to your firefox plugin folder
+mkdir -p ~/.mozilla/plugins/
+cp -v bin/linphone/nplinphone.so ~/.mozilla/plugins/
