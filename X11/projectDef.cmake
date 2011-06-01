b5f1af05d54693bf05dc150e48662e5cc8a16940
Jan Dolecek <juzna.cz@gmail.com>
2011-06-02 00:46:17 +0100

    fixed dependancies on ubuntu

--- X11/projectDef.cmake
+++ X11/projectDef.cmake
@@ -67,18 +67,17 @@ target_link_libraries(${PROJECT_NAME}
     ${MORE_LIBS}
     )
 
-SET(CPACK_GENERATOR "${CPACK_GENERATOR};DEB")
+SET(CPACK_GENERATOR "DEB")
 SET(CPACK_PACKAGE_VENDOR "juzna.cz")
 SET(CPACK_DEBIAN_PACKAGE_NAME linphone-js)
 SET(CPACK_DEBIAN_PACKAGE_VERSION 0.1)
 SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Jan Dolecek <juzna.cz@gmail.com>")
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Linphone wrapper for JavaScript")
-
-SET(DEPS_MEDIASTREAMER "libasound2 (>> 1.0.22), libavcodec52 (>= 4:0.5+svn20090706-3) | libavcodec-extra-52 (>= 4:0.5+svn20090706-3), libgsm1 (>= 1.0.13), libogg0 (>= 1.0rc3), libsdl1.2debian (>= 1.2.10-1), libspeex1 (>= 1.2~beta3-1), libspeexdsp1 (>= 1.2~beta3.2-1), libswscale0 (>= 4:0.5+svn20090706-3) | libswscale-extra-0 (>= 4:0.5+svn20090706-3), libtheora0 (>= 0.0.0.alpha7.dfsg), libx11-6")
-SET(DEPS_CORE "libspeex1, libosip2-4, libexosip2-4")
+SET(DEPS_MEDIASTREAMER "libasound2 (>> 1.0.22), libavcodec52 (>= 4:0.5+svn20090706-3) | libavcodec-extra-52 (>= 4:0.5+svn20090706-3), libgsm1 (>= 1.0.13), libogg0 (>= 1.0rc3), libsdl1.2debian (>= 1.2.10-1), libspeexdsp1 (>= 1.2~beta3.2-1), libswscale0 (>= 4:0.5+svn20090706-3) | libswscale-extra-0 (>= 4:0.5+svn20090706-3), libtheora0 (>= 0.0.0.alpha7.dfsg), libx11-6")
+SET(DEPS_CORE "libspeex1, libosip2-4, libexosip2-4, libortp8")
 SET(CPACK_DEBIAN_PACKAGE_DEPENDS "${DEPS_MEDIASTREAMER},${DEPS_CORE}")
-
+SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "libmediastreamer-dev")
 
 # file(INSTALL ${LINPHONE_LIBS} DESTINATION lib)
 
@@ -89,9 +88,9 @@ SET(LINPHONE_LIBS_R
 	${LINPHONE_CORE_PATH}/mediastreamer2/src/.libs/libmediastreamer.so
 	${LINPHONE_CORE_PATH}/mediastreamer2/src/.libs/libmediastreamer.so.1
 	${LINPHONE_CORE_PATH}/mediastreamer2/src/.libs/libmediastreamer.so.1.0.0
-	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so
-	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so.8
-	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so.8.0.0
+#	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so
+#	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so.8
+#	${LINPHONE_CORE_PATH}/oRTP/src/.libs/libortp.so.8.0.0
 )
 INSTALL(PROGRAMS ${LINPHONE_LIBS_R} DESTINATION lib)
 
